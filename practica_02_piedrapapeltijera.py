34f771df8e82463dad1cc2972ff13d9e1f6247e9
LezanaCode <juan.e.lezana@gmail.com>
2024-07-21 21:42:32 -0300

    Fin del juego, piedra papel tijera

--- practica_02_piedrapapeltijera.py
+++ practica_02_piedrapapeltijera.py
@@ -3,20 +3,34 @@
 # Lo ingresado por el usuario sea lowercase de tal forma de comparar minuscula con minuscula
 # Mas de 1 turno con el bucle while
 
+from math import fabs
+import os
+def limpiar_consola():
+    os.system('cls')
+
+limpiar_consola()
+
+
 nombre1 = input("¿Como se llamará el jugador 1?: ")
 nombre2 = input("¿Como se llamará el jugador 2?: ")
 
-jugador1 = input("¡Hola " + nombre1 + "! ¿Que eliges? ¿Piedra, Papel o tijeras?: ")
-jugador2 = input("¡Hola " + nombre2 + "! ¿Que eliges? ¿Piedra, Papel o tijeras?: ")
+while True:
+    jugador1 = input("¡Hola " + nombre1 + "! ¿Que eliges? ¿Piedra, Papel o tijeras?: ")
+    jugador2 = input("¡Hola " + nombre2 + "! ¿Que eliges? ¿Piedra, Papel o tijeras?: ")
 
-condicion1 = jugador1 == "piedra" and jugador2 == "tijera"
-condicion2 = jugador1 == "papel" and jugador2 == "piedra"
-condicion3 = jugador1 == "tijera" and jugador2 == "papel"
+    condicion1 = jugador1 == "piedra" and jugador2 == "tijera"
+    condicion2 = jugador1 == "papel" and jugador2 == "piedra"
+    condicion3 = jugador1 == "tijera" and jugador2 == "papel"
 
 
-if jugador1 == jugador2:
-    print("¡Ha sido un empate!")
-elif condicion1 or condicion2 or condicion3:
-    print("Ha ganado:", nombre2)
-else:
-    print("Ha ganado:", nombre2)
+    if jugador1 == jugador2:
+        print("¡Ha sido un empate!")
+    elif condicion1 or condicion2 or condicion3:
+        print("Ha ganado:", nombre1)
+    else:
+        print("Ha ganado:", nombre2)
+
+    respuesta = input(" \n¿Desea seguir jugando (s/n?: ")
+    if respuesta.lower() != 's':
+        print("Hasta pronto...")
+        break
